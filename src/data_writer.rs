9fd4fb2810f76ff1d572ad744e8f37ddaa5c383f
peyha <peyha.nicole@outlook.fr>
2024-03-09 22:51:30 +0100

    feat: add tx columns

--- src/data_writer.rs
+++ src/data_writer.rs
@@ -1,6 +1,8 @@
 use crate::Data;
 use anyhow::Result;
-use starknet::core::types::Transaction;
+use starknet::core::types::{
+    DeclareTransaction, DeployAccountTransaction, InvokeTransaction, Transaction,
+};
 
 pub fn write_data(data: Data, path: &str) -> Result<()> {
     let mut wtr = csv::Writer::from_path(path)?;
@@ -37,21 +39,108 @@ pub fn write_data(data: Data, path: &str) -> Result<()> {
             }
         }
         Data::Transactions(txs) => {
-            wtr.write_record(&["block_number", "tx_hash", "tx_type"])?;
-            for tx in txs {
-                let tx_type = match tx.0 {
-                    Transaction::Invoke(_) => "invoke",
-                    Transaction::L1Handler(_) => "l1handler",
-                    Transaction::Declare(_) => "declare",
-                    Transaction::Deploy(_) => "deploy",
-                    Transaction::DeployAccount(_) => "deploy_account",
+            // todo handle custom tx type info
+            wtr.write_record(&[
+                "block_number",
+                "tx_hash",
+                "tx_type",
+                "tx_type_version",
+                "nonce",
+                "caller",
+            ])?;
+            for (tx, block_number) in txs {
+                let record = match tx {
+                    Transaction::Invoke(InvokeTransaction::V0(sub_tx)) => [
+                        block_number.to_string(),
+                        format!("0x{:x}", sub_tx.transaction_hash),
+                        "Invoke".to_string(),
+                        "V0".to_string(),
+                        "None".to_string(),
+                        "None".to_string(),
+                    ],
+                    Transaction::Invoke(InvokeTransaction::V1(sub_tx)) => [
+                        block_number.to_string(),
+                        format!("0x{:x}", sub_tx.transaction_hash),
+                        "Invoke".to_string(),
+                        "V1".to_string(),
+                        sub_tx.nonce.to_string(),
+                        format!("0x{:x}", sub_tx.sender_address),
+                    ],
+                    Transaction::Invoke(InvokeTransaction::V3(sub_tx)) => [
+                        block_number.to_string(),
+                        format!("0x{:x}", sub_tx.transaction_hash),
+                        "Invoke".to_string(),
+                        "V3".to_string(),
+                        sub_tx.nonce.to_string(),
+                        format!("0x{:x}", sub_tx.sender_address),
+                    ],
+                    Transaction::L1Handler(sub_tx) => [
+                        block_number.to_string(),
+                        format!("0x{:x}", sub_tx.transaction_hash),
+                        "L1Handler".to_string(),
+                        sub_tx.version.to_string(),
+                        sub_tx.nonce.to_string(),
+                        "None".to_string(),
+                    ],
+                    Transaction::Declare(DeclareTransaction::V0(sub_tx)) => [
+                        block_number.to_string(),
+                        format!("0x{:x}", sub_tx.transaction_hash),
+                        "Declare".to_string(),
+                        "V0".to_string(),
+                        "None".to_string(),
+                        format!("0x{:x}", sub_tx.sender_address),
+                    ],
+                    Transaction::Declare(DeclareTransaction::V1(sub_tx)) => [
+                        block_number.to_string(),
+                        format!("0x{:x}", sub_tx.transaction_hash),
+                        "Declare".to_string(),
+                        "V1".to_string(),
+                        sub_tx.nonce.to_string(),
+                        format!("0x{:x}", sub_tx.sender_address),
+                    ],
+                    Transaction::Declare(DeclareTransaction::V2(sub_tx)) => [
+                        block_number.to_string(),
+                        format!("0x{:x}", sub_tx.transaction_hash),
+                        "Declare".to_string(),
+                        "V2".to_string(),
+                        sub_tx.nonce.to_string(),
+                        format!("0x{:x}", sub_tx.sender_address),
+                    ],
+                    Transaction::Declare(DeclareTransaction::V3(sub_tx)) => [
+                        block_number.to_string(),
+                        format!("0x{:x}", sub_tx.transaction_hash),
+                        "Declare".to_string(),
+                        "V1".to_string(),
+                        sub_tx.nonce.to_string(),
+                        format!("0x{:x}", sub_tx.sender_address),
+                    ],
+                    Transaction::Deploy(sub_tx) => [
+                        block_number.to_string(),
+                        format!("0x{:x}", sub_tx.transaction_hash),
+                        "Deploy".to_string(),
+                        sub_tx.version.to_string(),
+                        "None".to_string(),
+                        "None".to_string(),
+                    ],
+                    Transaction::DeployAccount(DeployAccountTransaction::V1(sub_tx)) => [
+                        block_number.to_string(),
+                        format!("0x{:x}", sub_tx.transaction_hash),
+                        "DeployAccount".to_string(),
+                        "V1".to_string(),
+                        sub_tx.nonce.to_string(),
+                        "None".to_string(),
+                    ],
+                    Transaction::DeployAccount(DeployAccountTransaction::V3(sub_tx)) => [
+                        block_number.to_string(),
+                        format!("0x{:x}", sub_tx.transaction_hash),
+                        "DeployAccount".to_string(),
+                        "V3".to_string(),
+                        sub_tx.nonce.to_string(),
+                        "None".to_string(),
+                    ],
                 };
 
-                wtr.serialize(&[
-                    format!("{}", tx.1),
-                    format!("{}", tx.0.transaction_hash()),
-                    tx_type.to_string(),
-                ])?;
+                wtr.serialize(&record)?;
             }
         }
         Data::None => (),
