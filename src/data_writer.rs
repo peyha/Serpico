16695f211085eb8571c1931b1746838511f171c7
peyha <peyha.nicole@outlook.fr>
2024-03-09 22:21:40 +0100

    feat: add block columns

--- src/data_writer.rs
+++ src/data_writer.rs
@@ -7,11 +7,32 @@ pub fn write_data(data: Data, path: &str) -> Result<()> {
 
     match data {
         Data::Blocks(blocks) => {
-            wtr.write_record(&["block_number", "block_timestamp"])?;
+            wtr.write_record(&[
+                "status",
+                "block_hash",
+                "parent_hash",
+                "block_number",
+                "new_root",
+                "timestamp",
+                "sequencer_address",
+                "l1_gas_price_in_fri",
+                "l1_gas_price_in_wei",
+                "starknet_version",
+                "tx_count",
+            ])?;
             for block in blocks {
                 wtr.serialize(&[
-                    format!("{}", block.block_number),
-                    format!("{}", block.timestamp),
+                    format!("{:?}", block.status),
+                    format!("0x{:x}", block.block_hash),
+                    format!("0x{:x}", block.parent_hash),
+                    block.block_number.to_string(),
+                    format!("0x{:x}", block.new_root),
+                    block.timestamp.to_string(),
+                    format!("0x{:x}", block.sequencer_address),
+                    block.l1_gas_price.price_in_fri.to_string(),
+                    block.l1_gas_price.price_in_wei.to_string(),
+                    block.starknet_version,
+                    block.transactions.len().to_string(),
                 ])?;
             }
         }
