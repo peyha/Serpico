2ec20cfe88b3bd68fb622dc233947fc55abc26fe
peyha <peyha.nicole@outlook.fr>
2024-03-24 13:54:05 +0100

    feat: data writer error handling

--- src/error.rs
+++ src/error.rs
@@ -1,8 +1,12 @@
+use csv::Error;
 use starknet::providers::ProviderError;
+use std::io;
 use url::ParseError;
 
 #[derive(Debug)]
 pub enum SerpicoError {
     UrlParsingErr(ParseError),
     ClientErr(ProviderError),
+    WriterErr(Error),
+    IoErr(io::Error),
 }
